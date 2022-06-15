a1010ef8bcb56cfb90ad073fe689df6602c4b2ac
EdDaPrez <107413202+EdDaPrez@users.noreply.github.com>
2022-06-15 16:03:41 -0500

    Update montySim.py
    updated

--- montySim.py
+++ montySim.py
@@ -1,38 +1,48 @@
-print("Monty Hall Simulation \n")
-print("You are a participant in a gameshow and are presented with 3 doors by the host.\nBehind one door, there is a brand new car. But")
-
-import time
-import random
-
 totalCounter = 0 #keeps count of the iteration of the loop
 finalTime = 0 #time that loop uses to determine whether to terminate or not
-swapDoor = 0 #keeps count of the amount of cars earned after swapping doors
-noSwapDoor = 0 #keeps count of the amount of cars earned without swapping doors
+swapDoorW = 0 #keeps count of the amount of cars earned after swapping doors
+swapDoorL = 0
+noSwapDoorW = 0 #keeps count of the amount of cars earned without swapping doors
+noSwapDoorL = 0
 secondChoice = 0 #global variable created to be referenced within the loop
-doorList = 0
-carChoice = 0
+userSecondChoice = 0
+carDoor = 0
 userChoice = 0
 firstChoice = 0
 secondChoice = 0
+indexCount = 0
 
 initialTime = time.perf_counter()
 while (finalTime-initialTime < 5):
-    doorList = [0,1,2]
-    carChoice = random.randint(0,2)
-    doorList.remove(carChoice)
-    userChoice = random.randint(0,2)
-    if userChoice != carChoice:
-        doorList.remove(userChoice)
-    firstChoice = random.choice(doorList)
-    secondChoice = random.randint(0,1)#0 is to swap, 1 is not to swap
-    if userChoice != carChoice:
-        swapDoor = swapDoor + 1
+    carDoor = random.randint(1,3)
+    userChoice = random.randint(1,3)
+    hostOptionList = []
+    indexCount = 0
+    for i in range(0, 2):
+        if i + 1 != carDoor and i + 1 != userChoice:
+            hostOptionList[indexCount] = (i + 1)
+            indexCount = indexCount + 1
+        i = i + 1
+    firstChoice = random.choice(hostOptionList)
+    userSecondChoice = [1,2,3]
+    doorList.remove(firstChoice)
+    secondChoice = random.choice(doorList)#0 is to swap, 1 is not to swap
+    if secondChoice == 0:
+        if userChoice != carDoor:
+            swapDoorW = swapDoorW + 1
+        else:
+            swapDoorL = swapDoorL + 1
     else:
-        noSwapDoor = noSwapDoor + 1
+        if userChoice == carDoor:
+            noSwapDoorW = noSwapDoorW + 1
+        else:
+            noSwapDoorL = noSwapDoorL + 1
     totalCounter = totalCounter + 1
     finalTime = time.perf_counter()
-print(str(totalCounter) + " total iterations")
-print(str(swapDoor) + " cars could be earned after swapping doors")
-print(str(noSwapDoor) + " cars could be earned without swapping doors")
 
-print("This means that if you decide to swap doors, you have a " + str((swapDoor/totalCounter)*100) + "% chance of getting a car.\n Meanwhile, when choosing to not switch doors, you have a " + str((noSwapDoor/totalCounter)*100) + "% chance of getting a car.")
+print(str(totalCounter) + " total iterations")
+print(str(swapDoorW) + " times won after swapping doors")
+print(str(swapDoorL) + " times lost after swapping doors")
+print(str(noSwapDoorW) + " times won after not swapping doors")
+print(str(noSwapDoorL) + " times lost after not swapping doors")
+
