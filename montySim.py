e01224fd44a7dc06f6d1a1c4887c01c81e953b50
carinachen35 <107414458+carinachen35@users.noreply.github.com>
2022-06-15 08:42:24 -0500

    Update montySim.py

--- montySim.py
+++ montySim.py
@@ -1,4 +1,17 @@
-print("Monte Hall Simulation \n")
-print("You are a participant in a gameshow and are presented with 3 doors by the host.\nBehind one door, there is a brand new car. But")
+import random
 
+possible_action = "door 1", "door 2", "door 3"
+car_position = random.choice(possible_action)
+print(f"car is behind {car_position}")
 
+if car_position == "door 1":
+  possible_goat_action = "door 2", "door 3"
+  goat_1_position = random.choice(possible_goat_action)
+  print(f"goat 1 is behind {goat_1_position}")
+  if goat_1_position == "door 2":
+    possible_goat2_action = "door 3"
+    print(f"goat 2 is bhind {possible_goat2_action}")
+
+if car_position == "door 2":
+  possible_goat3_action = "door 1", "door 3"
+  goat_1_position2 = random.choice()
