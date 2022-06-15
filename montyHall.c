6ea24008938bcc26d1dc0fbc037373ebd79396a8
EdDaPrez <107413202+EdDaPrez@users.noreply.github.com>
2022-06-15 16:19:19 -0500

    Update montyHall.c
    Certain things did not work, entirely reworking code.

--- montyHall.c
+++ montyHall.c
@@ -4,20 +4,49 @@
 #include <stdlib.h>
 
 //establish int variables
-int totalCounter = 0, swapDoor = 0, noSwapDoor = 0, secondChoice = 0, userChoice, firstChoice, randIndex, carChoice;
+int totalCounter = 0, swapDoor = 0, noSwapDoor = 0,
+secondChoice = 0, userInitialChoice, firstChoice, randIndex, 
+carChoice, i = 0, carDoor = 2;
 //establish time variable
 time_t finalTime;
+
+
+int main() {
+    srand(time(0));//randomizes results based on current time
+    time_t initialTime = time(NULL);
+    while((finalTime - initialTime) < 1) {
+        userInitialChoice = (rand() % (3));
+        carDoor = (rand() % (3));
+        
+    }
+}
 
 int main() {
     printf("Monty Hall Sim");
     time_t initialTime = time(NULL);
-    while ((finalTime - initialTime) < 5) {
-        int doorList[] = {0, 1, 2};
+    srand(time(0));//randomizes results based on current time
+    while ((finalTime - initialTime) < 1) {
         carChoice = (rand() % (3));
-        doorList.Remove(carChoice);
+        int goatDoors[2];
+        int newListIndex = 0;
+        for (i = 0; i < 3; i++) {
+            if (i != carChoice) {
+                goatDoors[newListIndex] = i;
+                newListIndex++
+            }   
+        }
         userChoice = (rand() % (3));
-        if (userChoice != carChoice) {
-            doorList.Remove(userChoice);
+        if userChoice != carChoice {
+            int hostOptions [1];
+        } else {
+            int hostOptions [2];
+        }
+        newListIndex = 0;
+        for (i = 0; i < 2; i++) {
+            if (goatDoors[i] != userChoice) {
+                hostOptions[newListIndex] = i;
+                newListIndex++;
+            }
         } 
         firstChoice = doorList[(rand() % (sizeof doorList))];//chooses random index of doors not containing car or chosen by the user
         secondChoice = (rand() % (2));//chooses either 0 or 1, 0 is swap, 1 is no swap
@@ -28,8 +57,9 @@ int main() {
         }
         totalCounter++;
         finalTime = time(NULL);
-}   }
-    printf("\n", totalCounter);
-    printf("\n", swapDoor);
-    printf("\n", noSwapDoor);
+        }
+    printf("\n%d", totalCounter);
+    printf("\n%d", swapDoor);
+    printf("\n%d", noSwapDoor);
     return 0;
+    }
