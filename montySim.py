e018b1be52664335a008e12a89e61c0f7b8ca38d
EdDaPrez <107413202+EdDaPrez@users.noreply.github.com>
2022-06-15 23:28:31 -0500

    Update montySim.py

--- montySim.py
+++ montySim.py
@@ -1,39 +1,39 @@
-print("Monty Hall Simulation \n")
-print("Wait 60 seconds...")
-
+#INITIALIZE
+#import libraries
 import time
 import random
 
-totalCounter = 0 #keeps count of the iteration of the loop
-finalTime = 0 #time that loop uses to determine whether to terminate or not
+#define variables
+totalCounter = 0 
+finalTime = 0 
 swapDoor = 0 #keeps count of the amount of cars earned after swapping doors
 noSwapDoor = 0 #keeps count of the amount of cars earned without swapping doors
-secondChoice = 0 #global variable created to be referenced within the loop
-userSecondChoice = 0
-carDoor = 0
-initialUserChoice = 0
-firstChoice = 0
-secondChoice = 0
-indexCount = 0
 
-initialTime = time.perf_counter()
-while (finalTime-initialTime < 60):
-    carDoor = random.randint(1,3)#random door car is behind
-    initialUserChoice = random.randint(1,3)#random door user chooses
-    hostOptionList = [] 
+#define functions
+def hostChoice():
+    hostOptionList = []
     for i in range(1, 4):
         if i != carDoor and i != initialUserChoice:
             hostOptionList.append(i)
-    hostChoice = random.choice(hostOptionList)
+    return random.choice(hostOptionList)
+
+#RUN
+#make sure program is running
+print("Monty Hall Simulation \n")
+print("Wait 60 seconds...")
+initialTime = time.perf_counter()
+while (finalTime-initialTime < 5): #loop ends when 1 minute reached
+    carDoor = random.randint(1,3)#random door car is behind
+    initialUserChoice = random.randint(1,3)#random door user chooses
     userOptions = [1, 2, 3]
-    userOptions.remove(hostChoice)
+    userOptions.remove(hostChoice())
     secondChoice = random.choice(userOptions)
     if secondChoice == initialUserChoice and carDoor == secondChoice:
         noSwapDoor = noSwapDoor + 1 
     elif secondChoice != initialUserChoice and carDoor == secondChoice:
         swapDoor = swapDoor + 1
-    totalCounter = totalCounter + 1
-    finalTime = time.perf_counter()
+    totalCounter = totalCounter + 1 #keeps count of the iteration of the loop
+    finalTime = time.perf_counter() #time that loop uses to determine whether to terminate or not
 print(str(totalCounter) + " total iterations")
 print(str(swapDoor) + " times won after swapping doors. Won ", str((swapDoor/(swapDoor + noSwapDoor)) * 100), "% of the time.")
 print(str(noSwapDoor) + " times won without swapping doors. Won ", str((noSwapDoor/(swapDoor + noSwapDoor)) * 100), "% of the time.")
