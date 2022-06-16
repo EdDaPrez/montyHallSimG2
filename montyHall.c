98032aac0ad2b1a1b8bcabb7a5ecf252c39b8a91
EdDaPrez <107413202+EdDaPrez@users.noreply.github.com>
2022-06-16 16:22:20 -0500

    Update montyHall.c

--- montyHall.c
+++ montyHall.c
@@ -2,31 +2,40 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <malloc.h>
 
 //establish int variables
-float dataPoints = 0, swapDoor = 0, noSwapDoor = 0, 
-userInitialChoice, i = 0, carDoor = 2;
+size_t swapDoor = 0, noSwapDoor = 0; 
+int carDoor = 2, userInitialChoice;
+size_t n = 36;
+
 //establish time variable
 time_t finalTime;
 
-
 int main() {
-    printf("Monty Hall\n");
+    int *swapDoor = malloc(n);
+    swapDoor = malloc(20);
+    int *noSwapDoor = malloc(n);
+    noSwapDoor = malloc(20);
+    printf("Monty Hall\n\nWait 60 seconds...\n\n");
     srand(time(0));//randomizes results based on current time
     time_t initialTime = time(NULL);//takes note of exact time loop started
-    while((finalTime - initialTime) < 1) {//stops loop at 60 sec
-        userInitialChoice = (rand() % (3));//random int betw 1-3
-        carDoor = (rand() % (3));//random int betw 1-3
-        if (carDoor != userInitialChoice){
+    while((time(NULL) - initialTime) < 5) {//stops loop at 60 sec
+        if (carDoor != (rand() % (3))){
             swapDoor++; //tracks wins when swapping
         } else {
             noSwapDoor++;//tracks wins without swapping
         }
-        dataPoints++;//tracks total iterations
-        finalTime = time(NULL);
     }
-    printf("%d iterations\n", dataPoints);
-    printf("%d cars won when door swapped\n", swapDoor);
-    printf("%d cars won when door not swapped", noSwapDoor);
+    int *dataPoints = malloc(n);
+    dataPoints = malloc(20);
+    dataPoints = *swapDoor + *noSwapDoor;
+    if (dataPoints < swapDoor){
+            printf("had overflow");
+    }
+    printf("%d datapoints\n", (dataPoints));
+    printf("%d cars won when door swapped\n", (swapDoor));
+    printf("%d cars won when door not swapped", (noSwapDoor));
+    free(dataPoints);
+    free(swapDoor);
+    free(noSwapDoor);
 }
