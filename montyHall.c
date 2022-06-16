f18319a9a0a21077b7438920006406ac15b30737
EdDaPrez <107413202+EdDaPrez@users.noreply.github.com>
2022-06-15 22:17:46 -0500

    Update montyHall.c

--- montyHall.c
+++ montyHall.c
@@ -2,34 +2,31 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 //establish int variables
-int totalCounter = 0, swapDoor = 0, noSwapDoor = 0, secondChoice = 0, userChoice, firstChoice, randIndex, carChoice;
+float dataPoints = 0, swapDoor = 0, noSwapDoor = 0, 
+userInitialChoice, i = 0, carDoor = 2;
 //establish time variable
 time_t finalTime;
 
+
 int main() {
-    printf("Monty Hall Sim");
-    time_t initialTime = time(NULL);
-    while ((finalTime - initialTime) < 5) {
-        int doorList[] = {0, 1, 2};
-        carChoice = (rand() % (3));
-        doorList.Remove(carChoice);
-        userChoice = (rand() % (3));
-        if (userChoice != carChoice) {
-            doorList.Remove(userChoice);
-        } 
-        firstChoice = doorList[(rand() % (sizeof doorList))];//chooses random index of doors not containing car or chosen by the user
-        secondChoice = (rand() % (2));//chooses either 0 or 1, 0 is swap, 1 is no swap
-        if (userChoice != carChoice) {
-            swapDoor++;
+    printf("Monty Hall\n");
+    srand(time(0));//randomizes results based on current time
+    time_t initialTime = time(NULL);//takes note of exact time loop started
+    while((finalTime - initialTime) < 1) {//stops loop at 60 sec
+        userInitialChoice = (rand() % (3));//random int betw 1-3
+        carDoor = (rand() % (3));//random int betw 1-3
+        if (carDoor != userInitialChoice){
+            swapDoor++; //tracks wins when swapping
         } else {
-            noSwapDoor++;
+            noSwapDoor++;//tracks wins without swapping
         }
-        totalCounter++;
+        dataPoints++;//tracks total iterations
         finalTime = time(NULL);
-}   }
-    printf("\n", totalCounter);
-    printf("\n", swapDoor);
-    printf("\n", noSwapDoor);
-    return 0;
+    }
+    printf("%d iterations\n", dataPoints);
+    printf("%d cars won when door swapped\n", swapDoor);
+    printf("%d cars won when door not swapped", noSwapDoor);
+}
